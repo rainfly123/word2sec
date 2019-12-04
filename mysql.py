8aac2f396c3cea199c6b45b96109dbcb34b2bed1
Your Name <you@example.com>
2019-12-04 12:28:33 +0800

    mysql

--- mysql.py
+++ mysql.py
@@ -5,7 +5,7 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 import datetime
 
-def Get(table_name):
+def Get():
     conn = pymysql.connect(
     host="localhost",
     user="root",
@@ -25,5 +25,27 @@ def Get(table_name):
     conn.close()
     return 
 
+def GetId(wid):
+    conn = pymysql.connect(
+    host="localhost",
+    user="root",
+    password= "123456",
+    database="outputs",
+    charset="utf8")
+ 
+ 
+    cursor = conn.cursor() 
+    sql = "select output from  outputs where id = %s"%(wid)
+ 
+    cursor.execute(sql)
+    mresults = cursor.fetchall()
+    for x in mresults:
+        print x[0]
+    cursor.close()
+    conn.close()
+    return 
+
+
 if __name__ == "__main__":
-    results = Get("outputs")
+    #results = Get("outputs")
+    print GetId(9000)
