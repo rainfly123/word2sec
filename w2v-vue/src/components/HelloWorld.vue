5f945341e8fc4ee9f6d1b4e3aacd72884e66e6a4
Your Name <you@example.com>
2019-12-04 11:35:21 +0800

    vue

---
+++ w2v-vue/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+<div style="margin-top:50px">
+<el-row>
+  <el-col :span="24"><div ><h2>
+  试题查重系统</h2></div></el-col>
+</el-row>
+<el-row>
+<el-col :offset="6" :span="12">
+  <div>
+  <br>
+  <el-input  type="textarea"
+  :rows="3" v-model="input" placeholder="请输入内容"></el-input>
+  <br>
+  <span class="demonstration">相似度阀值</span>
+    <el-slider v-model="similarity"></el-slider>
+  <br>
+  <el-button @click="query()" style="float:right;"type="primary">开始查询</el-button>
+  </div>
+ </el-col>
+</el-row>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+     msg: 'Welcome to Your Vue.js App',
+       input: '',
+       similarity:50
+    }
+  },
+ methods:{
+    query(){console.log(this.input, this.similarity)}
+ }
+}
+</script>
+
+<style>
+</style>
